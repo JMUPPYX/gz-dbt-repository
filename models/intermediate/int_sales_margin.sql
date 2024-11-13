9859ec453faacf7567f796d57b36e6276a262bda
Julie Mupavo <juliemupavo@gmail.com>
2024-11-13 08:58:38 +0000

    int_order_operationnal

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,16 +1,16 @@
-with
-source as (
-SELECT*
---purchase_cost= quantity*purchase_price
-,raw_sales.quantity*raw_product.purchase_price AS purchase_cost
-FROM
-{{ ref('stg_raw__product') }} AS raw_product
-LEFT JOIN
-{{ ref('stg_raw__sales') }} AS raw_sales
-USING (products_id)
-)
-SELECT *
---margin= revenue-purchase_cost
-,new_source.revenue - new_source.purchase_cost AS margin
-FROM
-source as new_source
+-- int_sales_margin
+
+
+
+SELECT
+    p.products_id,
+    s.date_date,
+    s.orders_id,
+    s.revenue,
+    s.quantity,
+    p.purchase_price,
+    ROUND(s.quantity * p.purchase_price, 2) AS purchase_cost,
+    ROUND(s.revenue - (s.quantity * p.purchase_price), 2) AS margin
+FROM {{ ref("stg_raw__sales") }} s
+LEFT JOIN {{ ref("stg_raw__product") }} p
+    ON s.pdt_id = p.products_id
